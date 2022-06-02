93a97c367ab0d64b297899fc7c716976e6791bd1
devYuMinKim <gimyumin40@gmail.com>
2022-06-02 16:45:29 +0900

    codeit express

---
+++ Desktop/cowork/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const app = express();
+
+const members = require('./members');
+
+app.get('/api/members', (req, res) => {
+    const { team } = req.query; // == const team = req.query.team;
+    if (team) {
+       const teamMembers = members.filter((m) => m.team === team);  // filter? : 
+       res.send(teamMembers);
+    } else {
+        res.send(members);
+    }
+});
+
+app.get('/api/members/:id', (req, res) => {
+    const { id } = req.params;
+    const member = members.find((m) => m.id === Number(id));
+    if (member) {
+        res.send(member);
+    } else {
+        res.status(404).send({ message: 'There is no such member' });
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Server is listening...');
+});
